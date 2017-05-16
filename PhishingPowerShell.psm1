6a1eb91b6b9b49d2777cbadd33dd0b7752466a5d
Chris Magnuson <hourglasssand@gmail.com>
2017-05-16 16:28:14 -0400

    Initial commit

---
+++ PhishingPowerShell.psm1
@@ -0,0 +1,26 @@
+﻿function Invoke-PhisingWebsiteProvision {
+    param (
+        $EnvironmentName
+    )
+    Invoke-ClusterApplicationProvision -ClusterApplicationName Phishing -EnvironmentName $EnvironmentName
+    $Nodes = Get-TervisClusterApplicationNode -ClusterApplicationName KafkaBroker -EnvironmentName $EnvironmentName
+}
+
+
+function Get-EmailAddressesToPhish {
+    $Mailbox = Get-O365Mailbox |
+    Add-Member -MemberType ScriptProperty -Name EmailAddressObjects -Value {
+        foreach ($EmailAddress in $this.EmailAddresses) {
+            $EmailAddressObjectParts = $EmailAddress -split ":"
+            [PSCustomObject]@{
+                Type = $EmailAddressObjectParts[0]
+                Address = $EmailAddressObjectParts[1]
+            }
+        }
+    } -PassThru
+
+    $SMTPAddresses = $Mailbox.EmailAddressObjects | 
+    where type -EQ smtp |
+    where Address -Match "Tervis.com"
+
+}
